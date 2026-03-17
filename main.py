8b89b2ad5f4eb0fa6a1968aa440f9691f6406449
Keegan Tu <keegantu@gmail.com>
2026-03-17 17:24:39 -0400

    Added error handling for invalid symbols

--- main.py
+++ main.py
@@ -36,38 +36,42 @@ async def get_stock(symbol: str):
         
    # overview_data = overview_response.json()
     global_quote_data = global_quote_response.json()
-    news_data = news_response.json()
 
-    print("Global Quote Response:", global_quote_data)
-    
-    news_list = []
+    if "Global Quote" in global_quote_data and "05. price" in global_quote_data["Global Quote"]:
+        news_data = news_response.json()
 
-    for article in news_data["articles"][:5]:
-        news_list_article = {'title': article["title"], 'description': article["description"], 'url': article["url"], 'published_at': article["publishedAt"], 'source': article["source"]["name"]}
-        news_list.append(news_list_article)
-
-    result = {
-        "symbol": symbol,
-        "quote": {
-            "price": global_quote_data["Global Quote"]["05. price"],
-            "open": global_quote_data["Global Quote"]["02. open"],
-            "high": global_quote_data["Global Quote"]["03. high"],
-            "low": global_quote_data["Global Quote"]["04. low"],
-            "volume": global_quote_data["Global Quote"]["06. volume"],
-            "previous_close": global_quote_data["Global Quote"]["08. previous close"],
-            "change": global_quote_data["Global Quote"]["09. change"],
-            "change_percent": global_quote_data["Global Quote"]["10. change percent"]
-        },
-
-        "news": news_list,
+        print("Global Quote Response:", global_quote_data)
         
-      #  "overview": {
-      #      "name": overview_data["Name"],
-       #     "description": overview_data["Description"]
-     #   }
-    }
-    
-    # Store in cache (expires in 5 minutes = 300 seconds)
-    r.setex(cache_key, 3000, json.dumps(result))
-    
-    return result
+        news_list = []
+
+        for article in news_data["articles"][:5]:
+            news_list_article = {'title': article["title"], 'description': article["description"], 'url': article["url"], 'published_at': article["publishedAt"], 'source': article["source"]["name"]}
+            news_list.append(news_list_article)
+
+        result = {
+            "symbol": symbol,
+            "quote": {
+                "price": global_quote_data["Global Quote"]["05. price"],
+                "open": global_quote_data["Global Quote"]["02. open"],
+                "high": global_quote_data["Global Quote"]["03. high"],
+                "low": global_quote_data["Global Quote"]["04. low"],
+                "volume": global_quote_data["Global Quote"]["06. volume"],
+                "previous_close": global_quote_data["Global Quote"]["08. previous close"],
+                "change": global_quote_data["Global Quote"]["09. change"],
+                "change_percent": global_quote_data["Global Quote"]["10. change percent"]
+            },
+
+            "news": news_list,
+            
+        #  "overview": {
+        #      "name": overview_data["Name"],
+        #     "description": overview_data["Description"]
+        #   }
+        }
+        
+        # Store in cache (expires in 5 minutes = 300 seconds)
+        r.setex(cache_key, 3000, json.dumps(result))
+        return result
+    else:
+        
+        return {"error": "Invalid stock symbol"}
